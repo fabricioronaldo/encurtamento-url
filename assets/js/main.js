ea8eef4d2b8b7587d66d36d606c3d6aacf490236
Fabricio Velasco <fabriciorvelasco@gmail.com>
2022-12-22 22:47:28 -0300

    Implementando API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,26 @@
 const url = document.querySelector("#url");
-
 const button = document.querySelector(".url__button")
+
+
+
+
+const showData = (result) => {
+    console.log(result)
+    const item = document.querySelector(".item__link")
+    item.textContent = result.result.full_share_link
+
+    /*for(const campo in result){
+        console.log(result.result.full_share_link
+            )
+    }*/
+
+}
 
 button.addEventListener("click", (evento) => {
     let search = url.value;
     fetch (`https://api.shrtco.de/v2/shorten?url=${search}`)
     .then(response => { response.json()
-        .then(data => console.log(data)) 
+        .then(data => showData(data)) 
     })
     .catch(evento => console.log('Ocorreu um erro!!!' + evento,message))
 });
