3a262c9b9968ea4c6427d9efd0adb53fda2c6ae4
Fabricio Velasco <fabriciorvelasco@gmail.com>
2022-12-28 22:15:28 -0300

    Finalização do projeto com as funcionalidades e responsividade

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 const url = document.querySelector("#url");
 const button = document.querySelector(".url__button");
 const result = document.querySelector("#result");
-const copy = document.querySelectorAll(".item__button");
+const copy = document.querySelector(".item__button");
+console.log(copy);
 const url_encu = document.querySelector(".item__link");
+console.log(url_encu);
 
-
-const showData = (result) => {
+/*const showData = (result) => {
     console.log(result)
     const link1 = document.querySelector(".link-1")
     const link2 = document.querySelector(".link-2")
@@ -26,16 +27,103 @@ button.addEventListener("click", (evento) => {
 });
 
 
-function copy_text() {
+/*function copy_text() {
     navigator.clipboard.writeText(url_encu.innerHTML);
-    copy.innerHTML = 'Copiado';
+    //copy[i].innerHTML = 'Copiado';
 }
 
-for ( var i = 0; i < copy.length; i++ ) {
+for ( i in copy ) {
     copy[i].addEventListener("click", copy_text);
+    
+}*/
+/*
+for ( i in copy ) {
+    copy.addEventListener("click", () => {
+        navigator.clipboard.writeText(url_encu.innerHTML);
+        copy[i].innerHTML = 'Copiado';
+    });
     
 }
 
-console.log(copy)
+copy.addEventListener('click', function (event) {
 
+    //let url = document.querySelector('.item__link');
+    url_encu.innerHTML;
+    console.log(url_encu);
+    document.execCommand('copy');
+
+});
+
+console.log(copy)*/
+
+
+button.addEventListener("click", (evento) => {
+    //result.classList.remove("none");
+    let search = url.value;
+    let link;
+    fetch (`https://api.shrtco.de/v2/shorten?url=${search}`)
+    .then(response => { response.json()
+        .then(data => showData(data))
+    })
+    .catch(evento => console.log('Ocorreu um erro!!!' + evento,message))
+});
+
+const showData = (result) => {
+    console.log(result)
+    const link = result.result.short_link2;
+    console.log(link);
+
+    let divResultItem = document.createElement("div");
+    divResultItem.className = "result__item"
+    
+    let linkOrigin = document.createElement("span");
+    linkOrigin.className = "item__link";
+    linkOrigin.innerHTML = url.value;
+    
+    let divResulItemChild = document.createElement("div");
+    
+    let linkShort = document.createElement("span");
+    linkShort.className = "item__link , item__link-short";
+    linkShort.innerHTML = link;
+    
+    let button = document.createElement("button");
+    button.className = "item__button";
+    button.innerHTML = "Copy";
+    
+    button.addEventListener('click', copyUrl);
+    
+
+    divResultItem.appendChild(linkOrigin);
+    divResultItem.appendChild(divResulItemChild);
+    divResulItemChild.appendChild(linkShort);
+    divResulItemChild.appendChild(button);
+
+    let local = document.getElementById("result");
+    local.after(divResultItem)
+}
+
+function copyUrl(event){
+    const e = event;
+    const copyLink = e.path[1].childNodes[0].innerText
+    console.log(copyLink);
+    navigator.clipboard.writeText(copyLink);
+    
+    const buttonCopy = e.path[0];
+
+    if (buttonCopy.innerHTML == "Copy") {
+
+        const innerButton = document.querySelectorAll('.item__button');
+        console.log(innerButton);
+        for (let i = 0; i < innerButton.length; i++) {
+            innerButton[i].innerHTML = "Copy";
+            innerButton[i].style.background = 'var(--Cyan)';
+        }
+
+        buttonCopy.innerHTML = "Copied";
+        buttonCopy.style.background = 'var(--Dark-Violet)';
+    } else {
+        buttonCopy.innerHTML = "Copy";
+        buttonCopy.style.background = 'var(--Cyan)';
+    }
+}
 
